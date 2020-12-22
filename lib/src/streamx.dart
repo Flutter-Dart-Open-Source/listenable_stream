8a167fe0157c407a7b2f7b7e78d7400113b2893a
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-12-22 11:55:34 +0700

    update startWith

--- lib/src/streamx.dart
+++ lib/src/streamx.dart
@@ -1,5 +1,4 @@
-import 'dart:async'
-    show MultiStreamController, Stream, StreamController, StreamSubscription;
+import 'dart:async' show Stream, StreamController, StreamSubscription;
 
 /// @private
 /// Prepends a value to the source [Stream].
@@ -7,59 +6,20 @@ extension StartWithExtension<T> on Stream<T> {
   /// @private
   /// Prepends a value to the source [Stream].
   Stream<T> startWith(T Function() seededProvider) {
-    MultiStreamController<T> controller;
-    StreamSubscription<T> subscription;
+    final controller = StreamController<T>(sync: true);
+    StreamSubscription subscription;
 
-    final listenUpStream = () => listen(
-          controller.addSync,
-          onError: controller.addErrorSync,
-          onDone: () {
-            subscription = null;
-            controller.closeSync();
-          },
-        );
-
-    final onListen = (MultiStreamController<T> c) {
-      if (controller != null) {
-        return;
-      }
-
-      controller = c;
-      controller.addSync(seededProvider());
-
-      subscription = listenUpStream();
-      controller.onCancel = () {
-        subscription?.cancel();
-        subscription = null;
-      };
+    controller.onListen = () {
+      controller.add(seededProvider());
+      subscription = listen(
+        controller.add,
+        onError: controller.addError,
+        onDone: controller.close,
+      );
     };
-
-    return Stream.multi(onListen, isBroadcast: false).toSingleSubscription();
-  }
-}
-
-/// @private
-/// Convert a [Stream] to Single-Subscription [Stream].
-extension ToSingleSubscriptionStreamExtension<T> on Stream<T> {
-  /// @private
-  /// Convert a [Stream] to Single-Subscription [Stream].
-  Stream<T> toSingleSubscription() {
-    StreamController<T> controller;
-    StreamSubscription<T> subscription;
-
-    controller = StreamController<T>(
-      sync: true,
-      onListen: () {
-        subscription = listen(
-          controller.add,
-          onError: controller.addError,
-          onDone: controller.close,
-        );
-      },
-      onPause: () => subscription.pause(),
-      onResume: () => subscription.resume(),
-      onCancel: () => subscription.cancel(),
-    );
+    controller.onPause = () => subscription.pause();
+    controller.onResume = () => subscription.resume();
+    controller.onCancel = () => subscription.cancel();
 
     return controller.stream;
   }
